37ac24c66ddab384c9b35201c45f93f726d6418f
“ch4qr1git config --global user.email <39025763+ch4qr1@users.noreply.github.com>
2021-03-18 00:31:40 +0300

    Revert "disable devfreq boost"
    This reverts commit df6c4cc6927a0438d7c1070c53928a5a9e4e47c1.

--- drivers/video/fbdev/exynos/dpu/decon_core.c
+++ drivers/video/fbdev/exynos/dpu/decon_core.c
@@ -2746,8 +2746,10 @@ static int decon_set_win_config(struct decon_device *decon,
 	if (ret)
 		goto err_prepare;
 
-	if (win_data->fence >= 0)
+	if (win_data->fence >= 0) {
+		devfreq_boost_kick(DEVFREQ_EXYNOS_MIF);
 		decon_install_fence(fence, win_data->fence);
+	}
 
 	decon_hiber_block(decon);
 
